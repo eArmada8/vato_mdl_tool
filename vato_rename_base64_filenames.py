948157df7b450024a9803fe5572651797cd51a84
eArmada8 <103985972+eArmada8@users.noreply.github.com>
2025-03-02 18:19:44 -0800

    Basic recursive search support

--- vato_rename_base64_filenames.py
+++ vato_rename_base64_filenames.py
@@ -1,7 +1,7 @@
 # Tool to rename files from Valkyrie Anatomia: The Origin.
 #
 # Usage:  Run by itself without commandline arguments and it will rename
-# every file it finds in the folder.
+# every file it finds via a recursive search.
 #
 # For command line options, run:
 # /path/to/python3 vato_rename_base64_filenames.py --help
@@ -17,8 +17,12 @@ if __name__ == "__main__":
     else:
         os.chdir(os.path.abspath(os.path.dirname(__file__)))
 
-    files = [x for x in glob.glob('*.*') if not x in glob.glob('*.py')]
+    files = [x for x in glob.glob('**/*.*', recursive = True) if not x[-3:] == '.py']
     for file in files:
+        foldername = os.path.dirname(file)
         filename, ext = os.path.splitext(os.path.basename(file))
+        #print("filename: {}".format(filename))
         real_filename = base64.b64decode(filename.encode("ascii"))
-        os.rename(filename+ext, real_filename.decode()+ext)
+        if not foldername == '':
+            foldername += '/'
+        os.rename(foldername + filename+ext, foldername + real_filename.decode()+ext)
